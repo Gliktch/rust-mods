da94586e683b6c6b92b4d87a09dd432c7de6493e
Matthew (Gliktch) <github-07IU02@gliktch.com>
2014-03-05 02:14:05 +1000

    0.3 Code Cleanup, non-functional framework with update function

--- mods.lua
+++ mods.lua
@@ -1,6 +1,6 @@
 PLUGIN.Title = "Mod Information"
 PLUGIN.Author = "Gliktch"
-PLUGIN.Version = "0.4"
+PLUGIN.Version = "0.3"
 PLUGIN.Description = "Displays a list of mods running on the server, and their basic settings."
 
 function PLUGIN:Init()
@@ -50,38 +50,7 @@ end
 
 function PLUGIN:CollectValues()
 
- -- Main server info
-  hostname = Rust.server.hostname
-  hostip   = Rust.server.ip
-  hostport = Rust.server.port
-
- -- Most common settings
-  sleep    = Rust.sleepers.on
-  pvp      = Rust.server.pvp
-  craftin  = Rust.crafting.instant
-
--- Further useful settings
-  maxplay  = Rust.server.maxplayers
-  airmin   = Rust.airdrop.min_players
-  craftts  = Rust.crafting.timescale
-  craftws  = Rust.crafting.workbench_speed
-  falldmg  = Rust.falldamage.enabled
-  daydur   = Rust.env.daylength
-  nightdur = Rust.env.nightlength
-  decaysec = Rust.decay.deploy_maxhealth_sec
-  durmult  = Rust.conditionloss.damagemultiplier
-  durmulta = Rust.conditionloss.armorhealthmult
-
--- Less common settings
-  voicedis = Rust.voice.distance
-  ctimeout = Rust.server.clienttimeout
-  srvgroup = Rust.server.steamgroup
-  tickrate = Rust.decay.decaytickrate
-  fallminv = Rust.falldamage.min_vel
-  fallmaxv = Rust.falldamage.max_vel
-  legstime = Rust.falldamage.injury_length
-  locktime = Rust.player.backpackLockTime
-  autotime = Rust.save.autosavetime
+-- to do
 
 end
 
@@ -112,17 +81,6 @@ end
 
 function PLUGIN:cmdMods( netuser, args )
 
-local decaytext = ""
-if decaysec == 43200 then
-  decaytext = "at the standard rate."
-elseif decaysec < 43200 then
-  decaytext = "FASTER, about " .. round(43200 / decaysec, 2) .. "x the normal rate."
-elseif decaysec > 43200 then
-  decaytext = "SLOWER, about 1/" .. round(decaysec / 43200, 2) .. "th the normal rate."
-end
+-- to do
 
-  rust.SendChatToUser( netuser, "Server: " .. hostname .. " (" .. tostring( #rust.GetAllNetUsers() ) .. "/" .. maxplay .. ")")
-  rust.SendChatToUser( netuser, "To connect manually, you can use use net.connect " .. hostip .. ":" .. hostport .. " in the F1 console.")
-  rust.SendChatToUser( netuser, "Sleepers are " .. (toboolean(sleep) and "ON" or "OFF") .. ", PVP is " .. (toboolean(pvp) and "ON" or "OFF") .. ", Fall Damage is " .. (toboolean(falldmg) and "ON" or "OFF") .. ", and Decay is " .. decaytext)
-  rust.SendChatToUser( netuser, "Instant crafting is " .. (toboolean(craftin) and "ON" or "OFF, but the Crafting Timescale is " .. craftts .. " and reduced by " .. craftws .. "x when at a Workbench."))
 end
